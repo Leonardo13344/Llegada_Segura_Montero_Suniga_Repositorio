ddcfceaf1c75f42f227f423789107f9cad154d4a
Anthonny Montero <heatkdwow@gmail.com>
2022-07-17 10:53:13 -0500

    PantallasRegistro

---
+++ app/src/main/java/com/example/llegadasegura/registro/Registro4.kt
@@ -0,0 +1,12 @@
+package com.example.llegadasegura.registro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.llegadasegura.R
+
+class Registro4: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro4)
+    }
+}
---
+++ app/src/main/java/com/example/llegadasegura/registro/Registro5.kt
@@ -0,0 +1,12 @@
+package com.example.llegadasegura.registro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.llegadasegura.R
+
+class Registro5: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro5)
+    }
+}
