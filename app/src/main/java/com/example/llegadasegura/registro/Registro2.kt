ddef82f897c82fb6f2134022155b0c07cc804476
AaMontero <105837025+AaMontero@users.noreply.github.com>
2022-07-24 01:35:02 -0500

    Conexion a la base de Datos 1

--- app/src/main/java/com/example/llegadasegura/registro/Registro2.kt
+++ app/src/main/java/com/example/llegadasegura/registro/Registro2.kt
@@ -3,21 +3,40 @@ package com.example.llegadasegura.registro
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.llegadasegura.databinding.ActivityRegistro2Binding
-
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Registro2 : AppCompatActivity() {
     private lateinit var binding: ActivityRegistro2Binding
-
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegistro2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnContinuar.setOnClickListener{
             cambiarPantallaRegistro3()
+            guardarDato()
         }
 
     }
+    val user = hashMapOf(
+        "first" to "Ada",
+        "last" to "Lovelace",
+        "born" to 1815
+    )
+    private fun guardarDato(){
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAG", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("TAG", "Error adding document", e)
+            }
+    }
+
     private fun cambiarPantallaRegistro3(){
         var intent = Intent(this, Registro3::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
